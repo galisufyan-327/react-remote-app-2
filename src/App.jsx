ec08c073cbde1e0ae6ab341d1ad920d5b700fad2
Ali Sufyan <alisufyan327@gmail.com>
2024-08-10 21:07:49 +0500

    change component for demo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {
   __federation_method_getRemote,
   __federation_method_setRemote,
 } from "__federation__";
-import { Provider, useDispatch } from "react-redux";
+import { Provider } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 
@@ -24,23 +24,13 @@ const DynamicRemoteStore = () => {
 
 const App = () => {
   const [store, setStore] = useState(null);
-  const dispatch = useDispatch();
 
   useEffect(() => {
     //@ts-ignore
     DynamicRemoteStore().then((storeModule) => {
-      dispatch({
-        type: "menu/replaceMenuItems",
-        payload: [
-          { text: "Dashboard", to: "/dashboard" },
-          { text: "Our Vision", to: "/vision" },
-          { text: "Contact", to: "/contact" },
-        ],
-      });
-
       setStore(storeModule.store);
     });
-  }, [dispatch]);
+  }, []);
 
   if (!store) {
     return <div>Loading...</div>;
@@ -50,16 +40,10 @@ const App = () => {
     <Provider store={store}>
       <Box component="main" sx={{ p: 5 }}>
         <Routes>
-          <Route path="/dashboard" element={<h1>Something on dashboard</h1>} />
-          <Route
-            path="/vision"
-            element={<h1>Our Vision for this project</h1>}
-          />
+          <Route path="/" element={<h1>Landing page</h1>} />
+          <Route path="/about" element={<button>Placeholder button</button>} />
 
-          {/* Using path="*"" means "match anything", so this route
-          acts like a catch-all for URLs that we don't have explicit
-          routes for. */}
-          <Route path="*" element={<h1>Page not not Found</h1>} />
+          <Route path="*" element={<h1>Fallback page</h1>} />
         </Routes>
       </Box>
     </Provider>
